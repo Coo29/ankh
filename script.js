44b7906d4f24d3887e3a71a96bfc41fb2e1cf8b9
Coo <80071732+Coo29@users.noreply.github.com>
2025-03-14 21:25:30 -0400

    added joker template to file.
    Various other minor changes, still not functional.

--- script.js
+++ script.js
@@ -1,17 +1,108 @@
 let templates = {}; // Store loaded templates
 
-// Function to load templates from a predefined list or uploaded files
+// Template loading here:
 async function loadTemplates() {
     const templateSelect = document.getElementById("templateSelect");
     
-    // Predefined templates (for simplicity, just one example)
-    templates['example'] = "Hello, {{name}}! You have {{items}} items.";
+    templates['example'] = `
+    Hello, {{name}}! You have {{items}} items.
+    `;
     
     // Add the predefined template to the dropdown
-    const option = document.createElement("option");
-    option.value = 'example';
-    option.textContent = 'Example Template';
-    templateSelect.appendChild(option);
+    const exampleOption = document.createElement("option");
+    exampleOption.value = 'example';
+    exampleOption.textContent = 'Example Template';
+    templateSelect.appendChild(exampleOption);
+
+    templates['jokers'] = `
+    SMODS.Joker { -- {{Name}} --
+       key = '{{Name}}',
+
+           -- description of the joker.
+        loc_txt = {
+            name = '{{Name}}',
+            text = {
+                "{{Description}}",
+            }
+        },
+
+           -- config of the joker. Variables go here.
+        config = {
+           extra = {
+                chips = '{{Chips}}',
+                chips_gain = '{{Chips_gain}}'
+                mult = '{{Mult}}',
+                mult_gain = '{{Mult_gain}}',
+                Xmult = '{{Xmult}}',
+                Xmult_gain = '{{Xmult_gain}}',
+                money = '{{Money}}',
+                money_gain = '{{Money_gain}}',
+
+         }
+     },
+            -- rarity level, 0 = common, 1 = uncommon, 2 = rare, 3 = legendary.
+        rarity = '{{Rarity}}',
+
+            -- atlas the joker uses for texture(s).
+        atlas = '{{Atlas}}',
+    
+            -- where on the atlas texture the joker is located.
+        pos = {
+            x = '{{Atlas_X}}',
+            y = '{{Atlas_Y}}'
+        },
+            -- cost of the joker in the shop.
+        cost = '{{Cost}}',
+
+            -- whether it is unlocked by default.
+        unlocked = '{{Unlocked_TF}}',
+
+            -- whether it is discovered by default.
+        discovered = '{{Discovered_TF}}',
+
+            -- whether blueprint can copy this joker.
+        blueprint_compat = '{{Compat_with_Blueprint_TF}}',
+
+            -- whether this joker can have the perishable sticker.
+        perishable_compat = '{{Compat_with_Perishable_TF}}',
+            -- whether this joker can have the eternal sticker.
+        eternal_compat = '{{Compat_with_Eternal_TF}}',
+
+            -- whether duplicates of this joker can appear in the shop by default.
+        allow_duplicates = '{{Allow_duplicates_TF}}',
+
+            -- loc_vars works with the config and gives you text variables to work with.
+            -- these are formatted as #n#, where n is the position in the variable table.
+        loc_vars = function(self, info_queue, card)
+            return {
+                vars = {
+                        -- #1#
+                    card.ability.extra.chips,
+                        -- #2#
+                    card.ability.extra.chips_gain,
+                        -- #3#
+                    card.ability.extra.mult,
+                        -- #4#
+                    card.ability.extra.mult_gain,
+                        -- #5#
+                    card.ability.extra.Xmult,
+                        -- #6#
+                    card.ability.extra.Xmult_gain,
+                        -- #7#
+                    card.ability.extra.money,
+                        -- #8#
+                    card.ability.extra.money_gain,
+                    }
+                }
+        end,
+    }
+    `;
+    
+    // Add the predefined template to the dropdown
+    const jokers = document.createElement("option");
+    jokers.value = 'jokers';
+    jokers.textContent = 'Jokers';
+    templateSelect.appendChild(jokers);
 }
 
 // Function to handle file upload and load custom template
@@ -58,10 +149,29 @@ function loadTemplate() {
         let label = document.createElement("label");
         label.textContent = displayName + ":";
 
-        let input = document.createElement("input");
-        input.type = "text";
-        input.id = varName;
-        input.placeholder = `Enter value for ${displayName}`;
+        let input;
+        
+        if (varName.endsWith("TF")) {
+            // Create a dropdown for variables ending with TF
+            input = document.createElement("select");
+            input.id = varName;
+            
+            let trueOption = document.createElement("option");
+            trueOption.value = "true";
+            trueOption.textContent = "True";
+            input.appendChild(trueOption);
+            
+            let falseOption = document.createElement("option");
+            falseOption.value = "false";
+            falseOption.textContent = "False";
+            input.appendChild(falseOption);
+        } else {
+            // Create a text input for other variables
+            input = document.createElement("input");
+            input.type = "text";
+            input.id = varName;
+            input.placeholder = `Enter value for ${displayName}`;
+        }
 
         wrapper.appendChild(label);
         wrapper.appendChild(input);
@@ -110,7 +220,10 @@ function exportScript() {
 }
 
 // Event listeners
-document.getElementById("templateSelect").addEventListener("change", loadTemplate);
+document.getElementById("templateSelect").addEventListener("change", () => {
+    loadTemplate();
+});
+
 document.getElementById("fileInput").addEventListener("change", handleFileUpload);
 document.getElementById("generateButton").addEventListener("click", generateScript);
 document.getElementById("exportButton").addEventListener("click", exportScript);
